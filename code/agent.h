b98bf9b4c77975bf505706e174a139346145ef62
Jordan Lewis <jordanthelewis@gmail.com>
2010-04-07 04:00:21 -0500

    Add first-cut agent and physics interfaces

---
+++ code/agent.h
@@ -0,0 +1,24 @@
+#ifndef AGENT_H
+#define AGENT_H
+
+#include "vector.h"
+
+class Agent
+{
+    bool is_braking;       /* Brake on or off? */
+    bool is_accelerating;  /* Accelerating or not? */
+    float turn_angle;      /* Angle of turn, 0 = no turn, radians */
+
+  public:
+    Agent (Vec3f_t);  /* Constructor; Initial position */
+    ~Agent ();        /* Destructor */
+
+    /* The below methods are the interface between the generic agent implemented
+     * in agent.c and the two types of agent controllers - AI or Input. */
+
+    void brake (bool);      /* Brake on or off? */
+    void accelerate (bool); /* Acceleration on or off? */
+    void turn (float);      /* Turning? Angle of turn, 0 = no turn */
+}
+
+#endif
---
+++ code/physics.h
@@ -0,0 +1,9 @@
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+namespace physics
+{
+    void simulate(); /* step the world forward. */
+}
+
+#endif
