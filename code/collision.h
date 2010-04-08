08cecc6a107a1b405fabd88f1dae374948132f77
Jordan Lewis <jordanthelewis@gmail.com>
2010-04-08 12:51:51 -0500

    Refactor collision functions into separate library

---
+++ code/collision.h
@@ -0,0 +1,8 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+namespace collision
+{
+    float ray_sphere(Rayf_t ray, Vec3f_t center, float radius); /* When does ray intersect sphere? */
+}
+
+#endif
--- code/physics.h
+++ code/physics.h
@@ -5,17 +5,7 @@
 
 namespace physics
 {
-    class collision {
-	float t; /* How far along the given ray our collision occurs */
-	int coll_id; /* Indentifier for the object that we collided with */
-    }
-
-
     void simulate(); /* step the world forward. */
-    class collision ray_collision(Ray_ft ray, float len); /* Test if collision
-							 will occur along this
-							 ray.  If so, return
-							 collision info */
 }
 
 #endif
