bdfab5035fc9bf4507366aa27c2a94922ff2912b
Youlian Simidjiyski <youlian@Sarah-Kerrigan.(none)>
2010-04-08 12:33:24 -0500

    Committer: Youlian Simidjiyski <youlian@Sarah-Kerrigan.(none)>  On branch master  Changes to be committed: 	modified:   physics.h 	Added collision testing interface

--- code/physics.h
+++ code/physics.h
@@ -1,9 +1,21 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include "vector.h"
+
 namespace physics
 {
+    class collision {
+	float t; /* How far along the given ray our collision occurs */
+	int coll_id; /* Indentifier for the object that we collided with */
+    }
+
+
     void simulate(); /* step the world forward. */
+    class collision ray_collision(Ray_ft ray, float len); /* Test if collision
+							 will occur along this
+							 ray.  If so, return
+							 collision info */
 }
 
 #endif
