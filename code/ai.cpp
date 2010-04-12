754e3cc09092bba6ef58883f4bc57b90aa6c5815
Jordan Lewis <jordanthelewis@gmail.com>
2010-04-11 21:11:33 -0500

    Add AIController and AIManager classes.
    An AIController has a pointer to the agent it controls. Calling its
    run() method gives new steering values to the agent it controls.
    An AIManager has a bunch of AIControllers. Its run method is what will
    be called by the scheduler, and it exists so we know what agents should
    be controlled by AI, as opposed to human input or network input.

---
+++ code/ai.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include "ai.h"
+#include "vector.h"
+#include "agent.h"
+
+AIController::AIController(Agent &agent)
+{
+    this->agent = &agent;
+}
+
+void AIManager::control(Agent &agent)
+{
+    AIController *newController = new AIController(agent);
+    controllers.push_back(newController);
+}
+
+void AIManager::release(Agent &agent)
+{
+    /* is there a less ugly way of doing an iterator loop like this? */
+    for (std::vector<AIController*>::iterator it = controllers.begin();
+         it != controllers.end(); ++it)
+    {
+        /* Look through our controller array, and remove the controller that
+         * has the agent with the ID of the input agent */
+        if ((*it)->agent->id == agent.id)
+        {
+            controllers.erase(it);
+            return;
+        }
+    }
+}
+
+void AIManager::run()
+{
+    for (unsigned int i = 0; i < controllers.size(); i++)
+    {
+        controllers[i]->run();
+    }
+}
--- code/ai.h
+++ code/ai.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "vector.h"
+#include "agent.h"
 
 /* \brief Class Path
  * \brief a path for an agent to follow
@@ -18,5 +19,25 @@ class Path
 
     Path *PathToPath(Vec3f_t position, float urgency);
 };
+
+class AIController
+{
+    /* cached AI data? paths? etc */
+  public:
+    Agent *agent;
+    void run(); /* !< Give new steering information to the agent we control */
+    AIController(Agent &);
+};
+
+class AIManager
+{
+    std::vector<AIController *> controllers; /* !< active AI controllers */
+
+  public:
+    void control(Agent &); /* !< assume AI control of given agent */
+    void release(Agent &); /* !< release AI control of given agent */
+    void run(); /* !< Give new steering information to each agent under our control*/
+};
+
 
 #endif
