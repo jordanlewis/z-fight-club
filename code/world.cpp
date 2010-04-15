4b0eb7c06044774b9338e56659e82ce38ca4c9b3
Matthew Steffen <msteffen@wireless-197-115.uchicago.edu>
2010-04-15 14:57:31 -0500

    Fixed changes to world.cpp

--- code/world.cpp
+++ code/world.cpp
@@ -1,1 +1,10 @@
 #include "world.h"
+
+float World::xMax = 1000; // XXX this probably will depend on tracks
+float World::zMax = 1000; // XXX this too
+
+World::World()
+{
+    environment = std::vector<Polygon>();
+    agents      = std::vector<Agent>();
+}
