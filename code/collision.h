f32cdac61c7ddb46473b03666a07b5a6c10d3c91
Youlian Simidjiyski <youlian@Sarah-Kerrigan.(none)>
2010-04-14 22:54:34 -0500

    modified:   Makefile 	modified:   collision.h 	modified:   physics.cpp 	modified:   physics.h 	modified:   world.h
    	Started to integrate ODE with our code

--- code/collision.h
+++ code/collision.h
@@ -3,9 +3,6 @@
 
 #include "vector.h"
 
-namespace collision
-{
-    float ray_sphere(Rayf_t ray, Vec3f_t center, float radius); /* When does ray intersect sphere? */
-}
+float ray_sphere(Rayf_t ray, Vec3f_t center, float radius); /* When does ray intersect sphere? */
 
 #endif
--- code/physics.h
+++ code/physics.h
@@ -1,6 +1,8 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include <ode/ode.h>
+
 #include "vector.h"
 #include "world.h"
 #include "agent.h"
@@ -14,6 +16,28 @@ class Physics
     Physics(World *world); /* constructor; initialize with a world */
     void initPhysics(World *world);
     void simulate(float dt); /* step the world forward by dt. */
+};
+
+/*A container grouping all of the various representations of an object in our
+  game world */
+class ObjectContainer
+{
+ private:
+    Kinematic *kinematic; //A kinematic bound to this object
+    dBodyID body; //an ODE body object that we will use to simulate physics
+    dMass mass;   //an ODE mass object giving us this object's mass info
+    dGeomID geom; //an ODE geom object used if our body is to be collidable
+    void write_kinematic(); //writes (syncs) the body coords into the kinematic
+
+ public:
+    
+    ObjectContainer();
+    /* Instantiates this ObjectContainer with a kinematic already bound */
+    ObjectContainer(Kinematic *kinematic);
+	/*binds a kinematic to this object. The kinematic data and body data
+	  should be kept in snyc thereafter*/
+    void bind_kinematic(Kinematic *kinematic);
+
 };
 
 #endif
