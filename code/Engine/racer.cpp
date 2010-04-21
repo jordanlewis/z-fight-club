493e24a53c2f538cba3bd526fa401f0a1ac8a9f5
Nick Russo <nick@cs.uchicago.edu>
2010-04-21 09:45:38 -0500

    committing so that I can finish unstashing

--- code/Engine/racer.cpp
+++ code/Engine/racer.cpp
@@ -62,8 +62,21 @@ int main(int argc, char *argv[])
     /* Tell input to send input to that playerController */
     input.controlPlayer(p);
 
-
-    // sound.schedule_sound("test.wav", GetTime()+10, Vec3f(0,0,0));
+    // sound.schedule_sound("808-cowbell.wav", GetTime()+2, Vec3f(0,0,0));
+    double now = GetTime();
+    double start = now+1;
+    for (int b = 0; b < 16; b++) // for sixteen beats
+    {
+        double t = b/4.0; // where each beat is a quarter of a second
+        if ((b%4) == 0)
+            sound.schedule_sound("808-bassdrum.wav", start+t, Vec3f(0,0,0));
+        if ((b%4) == 2)
+            sound.schedule_sound("808-hihat.wav", start+t, Vec3f(0,0,0));
+        if ((b%8) == 4)
+            sound.schedule_sound("808-clap.wav", start+t, Vec3f(0,0,0));
+        if ((b%8) == 0 || (b%8) == 7)
+            sound.schedule_sound("808-cowbell.wav", start+t, Vec3f(0,0,0));
+    }
 
     scheduler.loopForever();
 
