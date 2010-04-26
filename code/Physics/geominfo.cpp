4d074f4dfb3d2921445e9e5d9779f4b82edb30f6
Jordan Lewis <jordanthelewis@gmail.com>
2010-04-25 21:34:07 -0500

    Make GeomInfo.space a property of PObject instead.

--- code/Physics/geominfo.cpp
+++ code/Physics/geominfo.cpp
@@ -8,30 +8,31 @@ GeomInfo::GeomInfo(float bounce, float mu1, float mu2)
 {}
 
 PlaneInfo::PlaneInfo(float a, float b, float c, float d, 
-		     float bounce, float mu1, float mu2, dSpaceID space)
-    : GeomInfo(bounce, mu1, mu2), space(space), a(a), b(b), c(c), d(d)
+		     float bounce, float mu1, float mu2)
+    : GeomInfo(bounce, mu1, mu2), a(a), b(b), c(c), d(d)
 {}
 BoxInfo::BoxInfo(float lx, float ly, float lz, 
-		 float bounce, float mu1, float mu2, dSpaceID space)
-    : GeomInfo(bounce, mu1, mu2), space(space), lx(lx), ly(ly), lz(lz)
+		 float bounce, float mu1, float mu2)
+    : GeomInfo(bounce, mu1, mu2), lx(lx), ly(ly), lz(lz)
 {}
-SphereInfo::SphereInfo(float radius, 
-		       float bounce, float mu1, float mu2, dSpaceID space)
-    : GeomInfo(bounce, mu1, mu2), space(space), radius(radius)
+SphereInfo::SphereInfo(float radius, float bounce, float mu1, float mu2)
+    : GeomInfo(bounce, mu1, mu2), radius(radius)
 {}
 
 TriMeshInfo::TriMeshInfo(dTriMeshDataID meshID, const void * verts,
                          const void * tris, float bounce, float mu1,
-                         float mu2, dSpaceID space)
-    : GeomInfo(bounce, mu1, mu2), space(space), meshID(meshID),
-      verts(verts), tris(tris)
+                         float mu2)
+    : GeomInfo(bounce, mu1, mu2), meshID(meshID), verts(verts), tris(tris)
 {}
 
-dGeomID SphereInfo::createGeom() { return dCreateSphere (space, radius); }
-dGeomID BoxInfo::createGeom()    { return dCreateBox(space, lx, ly, lz); }
-dGeomID PlaneInfo::createGeom()  { return dCreatePlane(space, a, b, c, d); }
-dGeomID TriMeshInfo::createGeom(){ return dCreateTriMesh(space, meshID,
-                                                         NULL, NULL, NULL); }
+dGeomID SphereInfo::createGeom(dSpaceID space)
+{ return dCreateSphere (space, radius); }
+dGeomID BoxInfo::createGeom(dSpaceID space)
+{ return dCreateBox(space, lx, ly, lz); }
+dGeomID PlaneInfo::createGeom(dSpaceID space)
+{ return dCreatePlane(space, a, b, c, d); }
+dGeomID TriMeshInfo::createGeom(dSpaceID space)
+{ return dCreateTriMesh(space, meshID, NULL, NULL, NULL); }
 
 void SphereInfo::createMass(dMass * mass, float massVal)
 {
