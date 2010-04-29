9e9aa088e26f1a04d59b0dbe28cb517d1c10da14
Jordan Lewis <jordanthelewis@gmail.com>
2010-04-29 00:44:23 -0500

    Add simple implementation of GObject

---
+++ code/Graphics/gobject.cpp
@@ -0,0 +1,31 @@
+#include "gobject.h"
+#include "Engine/geominfo.h"
+#if defined(__APPLE__) && defined(__MACH__)
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#else
+#  define GL_GLEXT_PROTOTYPES
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#endif
+
+GObject::GObject(GeomInfo *geominfo) : geominfo(geominfo)
+{}
+
+void GObject::draw(Vec3f pos, Quatf_t quat)
+{
+    Mat4x4f_t matrix;
+    ToRotMatrix(quat, matrix);
+
+    matrix[12]=pos[0];
+    matrix[13]=pos[1];
+    matrix[14]=pos[2];
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glMultMatrixf(matrix);
+
+    geominfo->draw();
+
+    glPopMatrix();
+}
