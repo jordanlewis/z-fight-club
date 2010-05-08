849bcaa11937ce4817e3c29b7ff928363dcac18d
Youlian Simidjiyski <youlian@Sarah-Kerrigan.(none)>
2010-05-08 16:30:45 -0500

    Defined packet model in racerpacket.h/racerpacket.cpp, wrote 	createRacerPacket().  readRacerPacket will need to be user defined.
    	new file:   Network/racerpacket.cpp
    	new file:   Network/racerpacket.h

---
+++ code/Network/racerpacket.cpp
@@ -0,0 +1,10 @@
+#include "racerpacket.h"
+
+ENetPacket *makeRacerPacket(racerPacketType_t type, void *data, int size){
+
+    ENetPacket *packet = enet_packet_create(&type,
+					    sizeof(racerPacketType_t) + size,
+					    0);
+    memcpy(&(packet->data[sizeof(racerPacketType_t)]), data, size);
+    return packet;
+}
---
+++ code/Network/racerpacket.h
@@ -0,0 +1,21 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <enet/enet.h>
+#include <cstring>
+
+
+typedef enum {
+    START = 0,
+    AGENT
+} racerPacketType_t;
+
+/* makeRacerPacket:  Creates a RacerPacket.  RacerPackets begin with a
+ * racerPacketType_t describing the payload, followed by the payload itself.
+ * arg1:  packet type
+ * arg2:  pointer to packet payload (not counting the type)
+ * arg3:  size of packet payload (not counting the type)
+ */
+ENetPacket *makeRacerPacket(racerPacketType_t type, void *data, int size);
+
+#endif
