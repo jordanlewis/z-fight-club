3faeeb6310d273ac013620c70aa065d10b31684f
Nick Russo <nick@cs.uchicago.edu>
2010-05-19 17:12:26 -0500

    tweak welcome screen so I remember to hit enter comment out PlayerController output for now make Server::pushAgent private and don't profile it separately

--- code/Network/client.cpp
+++ code/Network/client.cpp
@@ -163,9 +163,9 @@ void Client::checkForPackets()
                         if (wo && wo->agent && wo->player)
                             {
                                 wo->player->ntoh(&P->info);
-                                cout << "PlayerController["
+                                /*cout << "PlayerController["
                                      << ntohl(P->ID) << "]: "
-                                     << *(wo->player) << endl;
+                                     << *(wo->player) << endl;*/
                                 wo->player->updateAgent();
                                 wo->agent->kinematic.ntoh(&(P->kine));
                             }
@@ -270,7 +270,6 @@ void Client::sendStartRequest()
 void Client::pushToServer()
 {
     error->pin(P_CLIENT);
-    error->log(NETWORK, TRIVIAL, "updating server\n");
     RPUpdateAgent payload;
     payload.ID = htonl(netID);
     if (player == NULL) return;
