48586044de8c94b0aca1edcd3b52bdeb2baf121f
Jordan Lewis <jordanthelewis@gmail.com>
2010-05-21 17:41:17 -0500

    Don't use PObjects in buildingtest

--- code/tests/setups/buildingtest.cpp
+++ code/tests/setups/buildingtest.cpp
@@ -2,7 +2,6 @@
 #include "Engine/setup.h"
 #include "Engine/world.h"
 #include "Engine/geominfo.h"
-#include "Physics/pobject.h"
 #include "Graphics/gobject.h"
 #include "Agents/kinematic.h"
 
@@ -11,42 +10,43 @@ void testSetup()
     World &world = World::getInstance();
 
     ObjMeshInfo *mesh = new ObjMeshInfo("building/");
-    Kinematic *k = new Kinematic(Vec3f(50.0, 0.0, -15.0), Vec3f(0,0,0), M_PI/2);
-    PMoveable *geom = new PMoveable(k, 10, mesh);
+    Vec3f pos = Vec3f(50,0,-15);
     GObject *gobj = new GObject(mesh);
-    WorldObject *wobj = new WorldObject(geom, gobj, NULL, NULL);
-
-    world.addObject(wobj);
-    
-    mesh = new ObjMeshInfo("building2/");
-    k = new Kinematic(Vec3f(79.0, 0.0, -17.0), Vec3f(0,0,0), M_PI/2);
-    geom = new PMoveable(k, 10, mesh);
-    gobj = new GObject(mesh);
-    wobj = new WorldObject(geom, gobj, NULL, NULL);
-
-    world.addObject(wobj);
-
-    mesh = new ObjMeshInfo("building/");
-    k = new Kinematic(Vec3f(112, 0.0, -5.0), Vec3f(0,0,0), M_PI/2);
-    geom = new PMoveable(k, 10, mesh);
-    gobj = new GObject(mesh);
-    wobj = new WorldObject(geom, gobj, NULL, NULL);
+    WorldObject *wobj = new WorldObject(NULL, gobj, NULL, NULL);
+    wobj->setPos(pos);
 
     world.addObject(wobj);
 
     mesh = new ObjMeshInfo("building2/");
-    k = new Kinematic(Vec3f(50.0, 0.0, 25.0), Vec3f(0,0,0), M_PI/2);
-    geom = new PMoveable(k, 10, mesh);
+    pos = Vec3f(79.0, 0.0, -17.0);
     gobj = new GObject(mesh);
-    wobj = new WorldObject(geom, gobj, NULL, NULL);
+    wobj = new WorldObject(NULL, gobj, NULL, NULL);
+    wobj->setPos(pos);
+
+    world.addObject(wobj);
+
+
+    mesh = new ObjMeshInfo("building/");
+    pos = Vec3f(112, 0.0, -5.0);
+    gobj = new GObject(mesh);
+    wobj = new WorldObject(NULL, gobj, NULL, NULL);
+    wobj->setPos(pos);
+
+    world.addObject(wobj);
+
+    mesh = new ObjMeshInfo("building2/");
+    pos = Vec3f(50.0, 0.0, 25.0);
+    gobj = new GObject(mesh);
+    wobj = new WorldObject(NULL, gobj, NULL, NULL);
+    wobj->setPos(pos);
 
     world.addObject(wobj);
     
     mesh = new ObjMeshInfo("building/");
-    k = new Kinematic(Vec3f(135.0, 0.0, 40.0), Vec3f(0,0,0), M_PI/2);
-    geom = new PMoveable(k, 10, mesh);
+    pos = Vec3f(135.0, 0.0, 40.0);
     gobj = new GObject(mesh);
-    wobj = new WorldObject(geom, gobj, NULL, NULL);
+    wobj = new WorldObject(NULL, gobj, NULL, NULL);
+    wobj->setPos(pos);
 
     world.addObject(wobj);
 
