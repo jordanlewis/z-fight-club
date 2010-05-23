cbf7ea965f305bdfc30c665c05bddf306bd8a12f
Jordan Lewis <jordanthelewis@gmail.com>
2010-05-23 01:52:39 -0500

    Break out path code into path.{h,cpp}

--- code/Agents/ai.cpp
+++ code/Agents/ai.cpp
@@ -5,6 +5,7 @@
 #include "ai.h"
 #include "Utilities/vec3f.h"
 #include "Utilities/error.h"
+#include "Utilities/path.h"
 #include "Engine/input.h"
 #include "agent.h"
 #include "Engine/world.h"
@@ -15,115 +16,7 @@
 #define ARC_RESOLUTION 20
 #define DEFAULT_PRECISION 7.0f
 
-Path::Path() : index(0) {}
-
 AIManager AIManager::_instance;
-
-Path::~Path() {}
-
-void Path::clear()
-{
-    knots.clear();
-    precision.clear();
-}
-
-void Path::next()
-{
-    if (++index >= knots.size())
-        index = 0;
-}
-
-static Vec3f closestPointOnSegment(Vec3f point, Vec3f enda, Vec3f endb)
-{
-    float proj;
-    Vec3f diff, seg, pnorm, closest;
-
-    diff = point - enda;
-    seg = endb - enda;
-    pnorm = seg / seg.length();
-    proj = pnorm.dot(diff);
-    if (proj > seg.length())
-        closest = endb;
-    else if (proj < 0)
-        closest = enda;
-    else
-        closest = pnorm * proj + enda;
-
-    return closest;
-}
-
-Vec3f Path::closestPoint(Vec3f point)
-{
-    float dist, bestDist = 10000;
-    Vec3f closest, bestClosest, next;
-    for (unsigned int i = 0; i < knots.size(); i++)
-    {
-        /* Compute distance from point to line segment knot -> nextKnot */
-        next = knots[(i + 1) % knots.size()];
-
-        closest = closestPointOnSegment(point, knots[i], next);
-
-        dist = (point - closest).length();
-
-        if (dist < bestDist)
-        {
-            bestDist = dist;
-            bestClosest = closest;
-        }
-    }
-    return bestClosest;
-}
-
-float Path::pointToDist(Vec3f point)
-{
-    Vec3f closest, next;
-    float len, dist, bestDist, pathTotal, finalDist;
-    bestDist = 1000000;
-    pathTotal = 0;
-    for (unsigned int i = 0; i < knots.size(); i++)
-    {
-        next = knots[(i + 1) % knots.size()];
-
-        len = (knots[i] - next).length();
-        closest = closestPointOnSegment(point, knots[i], next);
-        dist = (point - closest).length();
-
-        /* The smallest distance indicates we're on the closest segment to
-         * the point. So write the return value as the total value plus the
-         * segment-point distance here, and we'll return it if it is indeed
-         * the smallest distnace.*/
-        if (dist < bestDist)
-        {
-            bestDist = dist;
-            finalDist = pathTotal + (closest - knots[i]).length();
-        }
-
-        pathTotal += len;
-    }
-
-    return finalDist;
-}
-
-Vec3f Path::distToPoint(float dist)
-{
-    dist = fmodf(dist, totalLength);
-
-    float total, len;
-    Vec3f next, point;
-    for (unsigned int i = 0; i < knots.size(); i++)
-    {
-        next = knots[(i + 1) % knots.size()];
-
-        len = (knots[i] - next).length();
-        total += len;
-        if (total > dist)
-        {
-            point = lerp(knots[i], next, 1 - (total - dist) / len);
-            break;
-        }
-    }
-    return point;
-}
 
 Avoid::Avoid()
 {
--- code/Agents/ai.h
+++ code/Agents/ai.h
@@ -5,30 +5,8 @@
 #include <deque>
 #include "allclasses.h"
 #include "Utilities/vec3f.h"
+#include "Utilities/path.h"
 #include "Physics/collision.h"
-
-/* \brief Class Path
- * \brief a path for an agent to follow
- */
-class Path
-{
-  public:
-    float                totalLength; /* !< total distance along path */
-    unsigned int         index;     /* !< the first knot we haven't passed */
-    std::vector<Vec3f>   knots;     /* !< knots defining the path */
-    std::vector<float>   precision; /* !< how closely we want to follow the knots */
-    Path ();
-    Path (std::vector<Vec3f>);
-    ~Path();
-
-    void next();
-
-    Vec3f closestPoint(Vec3f point);/*!< return point on path closest to point*/
-    float pointToDist(Vec3f point);/*!<return distance on path of point */
-    Vec3f distToPoint(float dist);/*!<return point at distance along path */
-    Path *PathToPath(Vec3f position, float urgency);
-    void clear();
-};
 
 /* A target that needs to be avoided */
 class Avoid
