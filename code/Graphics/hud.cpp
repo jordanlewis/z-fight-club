fb3c7b5aac083d5b48db44cda85f1b4e0c9cc4fb
Jordan Lewis <jordanthelewis@gmail.com>
2010-05-25 15:46:35 -0500

    Clean up HUD

--- code/Graphics/hud.cpp
+++ code/Graphics/hud.cpp
@@ -40,7 +40,6 @@ Speedometer::Speedometer(Vec3f pos, Agent *agent)
 
 void Speedometer::draw()
 {
-    glDisable(GL_DEPTH_TEST);
     World &w = World::getInstance();
     DrawImage(background,
        w.camera.getWres() - background->wid - pos[0],
@@ -51,7 +50,7 @@ void Speedometer::draw()
     float theta = (agent->getKinematic().vel.length()*M_PI/15)-0.75;
     while(theta > (2*M_PI)) theta -= (2*M_PI);
     float center[2] = {105,105};
-    
+
     glColor3f(1,0,0);
     glBegin(GL_LINES);
       glVertex2f(
@@ -60,20 +59,6 @@ void Speedometer::draw()
       glVertex2f(
           world.camera.getWres()-center[0]-(100*cos(theta)),
           world.camera.getHres()-center[1]-(100*sin(theta)));
-    glEnd();
-    glEnable(GL_DEPTH_TEST);
-
-
-
-
-    if (agent->getKinematic().forwardSpeed() > 0)
-        glColor3f(0,1,0);
-    else
-        glColor3f(1,0,0);
-
-    glBegin(GL_LINES);
-    glVertex2f(20, world.camera.getHres() - 20);
-    glVertex2f(20, world.camera.getHres() - (20 + agent->getKinematic().vel.length() * 20));
     glEnd();
     glLineWidth(1);
 }
@@ -85,11 +70,11 @@ LapCounter::LapCounter(Vec3f pos, Agent *agent)
 void LapCounter::draw()
 {
     World &world = World::getInstance();
-    int ypos = world.camera.getHres() - 20;
+    int ypos = world.camera.getHres() - 10;
     stringstream ss;
     ss << "lap " << agent->lapCounter;
     glColor3f(0,0,1);
-    drawText(Vec3f(30, ypos, 0), ss.str(), NULL);
+    drawText(Vec3f(10, ypos, 0), ss.str(), NULL);
 }
 
 StopLight::StopLight(Vec3f pos) : Widget(pos), nLit(0)
@@ -160,10 +145,22 @@ MiniMap::MiniMap(Vec3f pos, Path *path) : Widget(pos), path(path)
 void MiniMap::draw()
 {
     World &world = World::getInstance();
-    float yoff = world.camera.getHres() - 100;
-    float xoff = 20;
+    float yoff = world.camera.getHres() - 85;
+    float xoff = 5;
     Vec3f vert;
     stringstream ss;
+
+    glEnable(GL_BLEND);
+    glColor4f(1,1,1,.4);
+    glBegin(GL_QUADS);
+    glVertex3f(0, world.camera.getHres(), 0);
+    glVertex3f(250, world.camera.getHres(), 0);
+    glVertex3f(250, world.camera.getHres() - 90, 0);
+    glVertex3f(0, world.camera.getHres() - 90, 0);
+    glEnd();
+    glDisable(GL_BLEND);
+
+    glLineWidth(3);
     glBegin(GL_LINE_LOOP);
     glColor3f(1,0,1);
     for (unsigned int i = 0; i < path->knots.size(); i++)
@@ -172,6 +169,7 @@ void MiniMap::draw()
         glVertex2f(vert.x + xoff, vert.z + yoff);
     }
     glEnd();
+    glLineWidth(1);
 
     glColor3f(0,0,1);
     for (unsigned int i = 0; i < world.wobjects.size(); i++)
@@ -183,7 +181,7 @@ void MiniMap::draw()
         vert.y = vert.z + yoff;
         ss.seekp(0);
         ss << world.wobjects[i]->agent->id;
-        drawText(vert, ss.str(), GLUT_BITMAP_HELVETICA_10);
+        drawText(vert, ss.str(), GLUT_BITMAP_HELVETICA_12);
     }
 }
 
@@ -201,6 +199,6 @@ void Places::draw()
         ss << aim.agentsSorted[i]->id << ", ";
     }
     glColor3f(0,0,1);
-    drawText(Vec3f(100, world.camera.getHres() - 20, 0), ss.str(),
+    drawText(Vec3f(75, world.camera.getHres() - 10, 0), ss.str(),
              GLUT_BITMAP_HELVETICA_18);
 }
