jordanlewis/z-fight-club

commit 8cd84846318eb730199e17ae3dab86ab8d834981

Fixed network bugs introduced by new code, introduced rudimentary 	network AI (still not working correctly), and added a world::makeCar() 	function to add a bit of modularity needed for networking.

code/Engine/world.cpp
@@ -358,34 +358,9 @@ Agent *World::placeAgent(int place)
 
     return agent;
 }
-/*
-//Creates a car unattached to any control structures.  Useful for networking.
+
+//Creates a car unattached to any control structures.
 Agent *World::makeCar()
-{
-    if (!track) 
-        return NULL;
-    Agent *agent = placeAgent(numAgents());
-    addAgent(agent);
-    return agent;
-}
-*/
-Agent *World::makeAI()
-{
-    if (!track)
-        return NULL;
-    AIManager &ai = AIManager::getInstance();
-    int nAgents = numAgents();
-    Agent *agent = placeAgent(nAgents);
-    addAgent(agent);
-    ai.control(agent);
-    ai.controllers.back()->lane((nAgents + 1) % 2);
-
-    ai.agentsSorted.push_back(agent);
-    return agent;
-}
-
-
-Agent *World::makePlayer()
 {
     if (!track)
         return NULL;
@@ -393,6 +368,27 @@ Agent *World::makePlayer()
     AIManager &aim = AIManager::getInstance();
     Agent *agent = placeAgent(numAgents());
     addAgent(agent);
+    aim.agentsSorted.push_back(agent);
+    return agent;
+}
+
+Agent *World::makeAI()
+{
+    if (!track)
+        return NULL;
+    AIManager &ai = AIManager::getInstance();
+    Agent *agent = makeCar();
+    ai.control(agent);
+    ai.controllers.back()->lane((numAgents() + 1) % 2);
+    return agent;
+}
+
+Agent *World::makePlayer()
+{
+    if (!track)
+        return NULL;
+
+    Agent *agent = makeCar();
     camera = Camera(THIRDPERSON, agent);
     Sound::getInstance().registerListener(&camera);
     PlayerController *p = new PlayerController(agent);
@@ -402,8 +398,6 @@ Agent *World::makePlayer()
     addWidget(s);
     LapCounter *lc = new LapCounter(Vec3f(0,0,0), agent);
     addWidget(lc);
-
-    aim.agentsSorted.push_back(agent);
 
     return agent;
 }

code/Engine/racer.cpp
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
         {
             world.PlayerQty = 1;
         }
-        if (world.runType == SOLO)
+        if (world.runType == SOLO || world.runType == SERVER)
         {
             if (vm.count("ai-players"))
             {

code/Engine/world.h
@@ -130,6 +130,7 @@ class World
     void addLight(Light *light);
     void addWidget(Widget *widget);
     Agent *placeAgent(int place);/*!<return a placed agent not yet in wobjects*/
+    Agent *makeCar(); //Create a car unattached to any control structures
     Agent *makeAI();
     Agent *makePlayer();
     void makeAgents();

code/Network/client.cpp
@@ -177,12 +177,13 @@ void Client::checkForPackets()
                       }
                     case RP_UPDATE_AGENT: 
                         {
-                            
                         error->log(NETWORK, TRIVIAL, "RP_UPDATE_AGENT\n");
                         RPUpdateAgent *P = (RPUpdateAgent *)payload;
+                        cout << "Updating agent " << ntohl(P->ID) << endl;
                         WorldObject *wo = netobjs[ntohl(P->ID)];
                         if (wo && wo->agent && wo->player && wo->pobject)
                             {
+                                cout << "entered if" << endl;
                                 wo->player->ntoh(&P->info);
                                 /*cout << "PlayerController["
                                      << ntohl(P->ID) << "]: "
@@ -211,21 +212,30 @@ void Client::checkForPackets()
                             error->log(NETWORK, TRIVIAL, "RP_CREATE_AGENT\n");
                             Agent *agent;
                             RPCreateAgent info = *(RPCreateAgent *)payload;
-
+                            player = &input->getPlayerController();
                             if (info.clientID == clientID)
                                 {
                                     error->log(NETWORK, TRIVIAL, "my ID!\n");
-                                    player = &input->getPlayerController();
                                     agent = world->makePlayer();
                                     Input::getInstance().controlPlayer(player);
+                                    netID = ntohl(info.netID);
                                 }
                             else 
                                 {
                                     error->log(NETWORK, TRIVIAL,"not my ID\n");
-                                    agent=world->placeAgent(world->numAgents());
-                                    world->addAgent(agent);
+                                    agent = world->makeCar();
+                                    new PlayerController(agent);
                                 }
                             
+                            attachNetID(agent->worldObject, ntohl(info.netID));
+                            break;
+                        }
+                    case RP_CREATE_AI_AGENT:
+                        {
+                            cout << "Server sez create AI!" << endl;
+                            error->log(NETWORK, TRIVIAL, "RP_CREATE_AGENT\n");
+                            RPCreateAIAgent info = *(RPCreateAIAgent *)payload;
+                            Agent *agent = world->makeAI();
                             attachNetID(agent->worldObject, ntohl(info.netID));
                             break;
                         }

code/Network/racerpacket.h
@@ -10,6 +10,7 @@ typedef enum {
     RP_START = 0, //Begin the race
     RP_JOIN, //Add a racer for my agent
     RP_CREATE_AGENT, //Create a net object with an attached agent 
+    RP_CREATE_AI_AGENT, //Create a net object with an attached ai agent
     RP_ATTACH_AGENT, //Attach an agent to an existing netobject
     RP_UPDATE_AGENT, //Update an existing agent 
     RP_KINEMATIC, //??????? What is this ????????
@@ -83,6 +84,10 @@ struct RPSteerInfo{
 struct RPCreateAgent {
     netObjID_t netID;
     uint8_t clientID;
+};
+
+struct RPCreateAIAgent {
+    netObjID_t netID;
 };
 
 struct RPKinematic{

code/Network/server.cpp
@@ -4,6 +4,7 @@
 #include "Engine/geominfo.h"
 #include "Agents/agent.h"
 #include "Agents/player.h"
+#include "Agents/ai.h"
 #include "Utilities/error.h"
 #include "Physics/pobject.h"
 #include "racerpacket.h"
@@ -95,12 +96,13 @@ netObjID_t Server::attachNetID(WorldObject *wobject, netObjID_t ID){
 }
 
 netObjID_t Server::createHumanAgent(uint8_t clientID){
-    World &world = World::getInstance();
-    Agent *agent = world.placeAgent(world.numAgents());
-    world.addAgent(agent);
+    Agent *agent = world->placeAgent(world->numAgents());
+    world->addAgent(agent);
     new PlayerController(agent);
-    cout << "Agent's wobject is now: " << agent->worldObject << endl;
+    AIManager::getInstance().agentsSorted.push_back(agent);
+
     netObjID_t netID = attachNetID(agent->worldObject);
+    
 
     if (NETOBJID_NONE == netID) {
         error->log(NETWORK, IMPORTANT, "WARNING: ran out of netIDs\n");
@@ -119,11 +121,32 @@ netObjID_t Server::createHumanAgent(uint8_t clientID){
 }
 
 netObjID_t Server::createAIAgent(){
+    error->log(NETWORK, TRIVIAL, "Creating an AI Agent\n");
+    Agent *agent = world->makeAI();
+    netObjID_t netID = attachNetID(agent->worldObject);
 
-    //    AIManager &ai = AIManager::getInstance();
+    if (NETOBJID_NONE == netID) {
+        error->log(NETWORK, IMPORTANT, "WARNING: ran out of netIDs\n");
+        return NETOBJID_NONE;
+    }
     
-    return NETOBJID_NONE;
+    struct RPCreateAIAgent toSend;
+    toSend.netID = htonl(netID);
+    ENetPacket *packet = makeRacerPacket(RP_CREATE_AI_AGENT, &toSend,
+                                         sizeof(RPCreateAIAgent),
+                                         ENET_PACKET_FLAG_RELIABLE);
+    toCreate.push_back(packet);
+    
+    return netID;
 }
+
+void Server::createAllAIAgents(){
+    for (int i = 0; i < world->AIQty; i++)
+        {
+            createAIAgent();
+        }
+}
+
 
 //should return NULL if unable to find an object with the given ID...
 WorldObject *Server::getNetObject(netObjID_t ID)
@@ -315,6 +338,8 @@ void Server::gatherPlayers()
     racerPacketType_t type;
     void * payload;
 
+    createAllAIAgents();
+
     while(1)
     {
         ENetEvent event;
@@ -488,6 +513,8 @@ void Server::serverFrame()
                         case RP_UPDATE_AGENT:
                             {
                                 RPUpdateAgent *P = (RPUpdateAgent *)payload;
+                                cout << "updating agent " << ntohl(P->ID)
+                                     << endl;
                                 WorldObject *wo = netobjs[ntohl(P->ID)];
                                 if (wo && wo->agent && wo->player)
                                 {

code/Network/server.h
@@ -62,6 +62,7 @@ class Server {
                                                                   * wobject.*/
     netObjID_t createHumanAgent(uint8_t clientID);
     netObjID_t createAIAgent();
+    void createAllAIAgents();
     
     
     void updateFromClient(uint8_t clientId); //NYI (call within serverFrame)

commit 5c464bb3b12ce00addfc75c9960aaeab051fb228

Fix bug in AI lane assignment caused in 8cd8484631

code/Engine/world.cpp
@@ -389,7 +389,7 @@ Agent *World::makeAI()
     AIManager &ai = AIManager::getInstance();
     Agent *agent = makeCar();
     ai.control(agent);
-    ai.controllers.back()->lane((numAgents() + 1) % 2);
+    ai.controllers.back()->lane((numAgents()) % 2);
     return agent;
 }
 