222f703418f1d5922261b885c0970bd374b92aae
Nick Russo <nick@cs.uchicago.edu>
2010-05-30 14:54:17 -0500

    increase attenuation of collisions and engine noises, and reduce effect of distance on attenuation.

--- code/Engine/world.cpp
+++ code/Engine/world.cpp
@@ -340,8 +340,8 @@ void World::addAgent(Agent *agent)
     SObject *sobj = NULL;
     if (!nosound)
     {
-        sobj = new SObject("18303_start.wav", GetTime(), AL_FALSE, 0.5);
-        sobj->registerNext(new SObject("18303_run.wav", 0, AL_TRUE, 0.5));
+        sobj = new SObject("18303_start.wav", GetTime(), AL_FALSE, 0.3);
+        sobj->registerNext(new SObject("18303_run.wav", 0, AL_TRUE, 0.3));
     }
     WorldObject *wobject = new WorldObject(pobj, gobj, sobj, agent);
     cout << "Agent's wobject pointer is: " << agent->worldObject << endl;
