9dc7adef2454ae6b7aab424f26d37b230ca84717
Jordan Lewis <jordanthelewis@gmail.com>
2010-07-25 16:59:02 -0500

    Document geominfo stuff

--- code/Engine/geominfo.h
+++ code/Engine/geominfo.h
@@ -43,26 +43,42 @@ typedef enum {
     OBJMESH
 } GeomInfoType_t;
 
-/* Stores info common to all geoms.  Cannot be constructed directly -- must be
-   constructed by a child class */
+/*! Abstract baseclass for representations of physical, possible drawable geometric objects.
+ *
+ * Subclasses of this class are meant to encapsulate geometrical information
+ * about shapes, for example spheres, cylinders, or planes, that help with
+ * construction meshes for the shapes, drawing them, and calculating their
+ * volumes.
+ *
+ * Subclasses must know how to create an ODE dMass for themselves, an ODE
+ * dGeomID for themselves, how to draw themselves (implemented in
+ * drawgeoms.cpp), and how to serialize and deserialize themselves for network
+ * send and receive.
+ */
 class GeomInfo
 {
  public:
+    /*! given an ODE space, create a dGeom that represents the shape of the
+     *  subclass, and return its dGeomID. */
     virtual dGeomID createGeom(dSpaceID space) = 0;
+    /*! given a pointer to an ODE dMass object and a mass value, populate the
+     *  dMass object with the given mass value. */
     virtual void createMass(dMass *, float) {return;};
-    /*! \brief draw() simply render the appropriate shape for this geom type.
+    /*! Render the appropriate shape for this geom type.
      * the caller will deal with position and orientation setup. */
     virtual void draw(Layer_t layer) {return;};
+    /*! Deserialize from payload. */
     virtual void ntoh(RPGeomInfo *payload) {return;};
+    /*! Serialize into payload. */
     virtual void hton(RPGeomInfo *payload) {return;};
 };
 
-//Packages info for a Sphere of radius r
+/*! A GeomInfo representing a sphere with a radius.*/
 class SphereInfo : public GeomInfo
 {
  public:
-    float radius;
-    SphereInfo(); //needed for networking
+    float radius; /*! The sphere's radius */
+    SphereInfo();
     SphereInfo(float radius);
     dGeomID createGeom(dSpaceID space);
     void createMass(dMass *, float);
@@ -71,12 +87,12 @@ class SphereInfo : public GeomInfo
     void hton(RPGeomInfo *payload);
 };
 
-//Packages info for a box of dimensions lx*ly*lz
+/*! A GeomInfo representing a rectangular prism. */
 class BoxInfo : public GeomInfo
 {
  public:
-    float lx, ly, lz;
-    BoxInfo(); //needed for networking
+    float lx, ly, lz; /*! The box's three side lengths.*/
+    BoxInfo();
     BoxInfo(float lx, float ly, float lz);
     dGeomID createGeom(dSpaceID space);
     void createMass(dMass *, float);
@@ -86,27 +102,37 @@ class BoxInfo : public GeomInfo
 };
 
 
-//Packages info for the plane defined by ax+by+cz = d;
+/*! A GeomInfo representing a flat plane.
+ *  planes don't have masses, and can't get drawn. */
+
 class PlaneInfo : public GeomInfo
 {
  public:
+    /*! <a,b,c> defines the normal vector of the plane.
+     *  d defines the plane's distance from the origin along the normal vector.
+     */
     float a, b, c, d;
-    PlaneInfo(); //needed for networking
+    PlaneInfo();
     PlaneInfo(float a, float b, float c, float d);
     dGeomID createGeom(dSpaceID space);
     void ntoh(RPGeomInfo *payload);
     void hton(RPGeomInfo *payload);
 };
 
-//Packages info for a ray of length len
+/*! A GeomInfo representing a ray. 
+ *
+ * Rays don't have mass, and can't get drawn. */
 class RayInfo : public GeomInfo
 {
  public:
-    float len;
+    float len; /*!< The ray's length. */
     RayInfo(float len);
     dGeomID createGeom(dSpaceID space);
 };
 
+/*! A GeomInfo representing an arbitrary triangle mesh, in ODE's format.
+ *
+ * These can be used to create ODE TriMesh physics objects. */
 class TriMeshInfo : public GeomInfo
 {
  public:
@@ -123,6 +149,8 @@ class TriMeshInfo : public GeomInfo
     void draw(Layer_t);
 };
 
+/*! A GeomInfo representing an arbirary triangle mesh with fancier graphics constructs.
+ * These can have textures and whatnot, unlike TriMeshInfos. */
 class ObjMeshInfo : public GeomInfo
 {
   private:
@@ -141,6 +169,7 @@ class ObjMeshInfo : public GeomInfo
 	void draw(Layer_t);
 };
 
+/*! A GeomInfo representing a textured skybox. */
 class SkyBoxInfo : public GeomInfo
 {
     public:
@@ -152,18 +181,25 @@ class SkyBoxInfo : public GeomInfo
         void draw(Layer_t);
 };
 
+/*! Represents a particle of a particle system. */
 class Particle
 {
     public:
-        Vec3f   pos;
-        Vec3f   vel;
-        float   ttl;
+        Vec3f   pos; /*!< The particle's position. */
+        Vec3f   vel; /*!< The particle's velocity. */
+        float   ttl; /*!< How long the particle has to live, in seconds. */
         Particle(Vec3f, Vec3f, float);
         ~Particle();
 };
 
-bool isDead(Particle *);
+bool isDead(Particle *); /*!< Returns true if the particle should die. */
 
+/*! A GeomInfo representing a particle system.
+ *
+ * A particle system is made up of a bunch of Particle objects, and some meta
+ * data about when the particles should be spawned, with what attributes, and
+ * when to remove those particles.
+ */
 class ParticleSystemInfo : public GeomInfo
 {
     public:
@@ -188,6 +224,7 @@ class ParticleSystemInfo : public GeomInfo
         void update(ParticleStreamObject *, float);
 };
 
+/*!< Creates an explosion effect with a particle system. */
 void makeExplosion(Vec3f, float);
 
 #endif
